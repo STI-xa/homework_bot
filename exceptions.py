4c30c30ec8dc3174b93996558ac0d2cab23f2ea4
Полина Николаева <sti-xa@ya.ru>
2022-11-22 20:22:53 +0300

    fixed bugs

--- exceptions.py
+++ exceptions.py
@@ -2,15 +2,8 @@
 
 class CommonErrors(Exception):
     """Общие ошибки, не требующие отправки сообщения в Телеграм."""
-
-    def __init__(self, text):
-        """Вохвращает название."""
-        self.text = text
-
+    pass
 
 class GeneralErorrs(Exception):
     """Важные ошибки, требующие отправки в Телеграм."""
-
-    def __init__(self, text):
-        """Вохвращает название."""
-        self.text = text
+    pass
--- homework.py
+++ homework.py
@@ -2,13 +2,14 @@ import logging
 import os
 import sys
 import time
-import exceptions
+from http import HTTPStatus
 from json import JSONDecodeError
 
 import requests
 import telegram
 from dotenv import load_dotenv
-from http import HTTPStatus
+
+import exceptions
 
 load_dotenv()
 
@@ -26,12 +27,12 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 600
+RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
@@ -45,61 +46,55 @@ def send_message(bot, message):
     except telegram.error.TelegramError as error:
         logging.error(f'Ошибка отправки сообщения {error}')
     else:
-        logging.info(f'Сообщение {message} отправлено')
+        logging.debug(f'Сообщение {message} отправлено')
 
 
-def get_api_answer(current_timestamp):
+def get_api_answer(timestamp):
     """Функция делает запрос к API сервиса.
     Возвращает ответ, преобразовывая данные к типам данных Python.
     """
-    timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
 
     try:
         homework_statuses = requests.get(
-            ENDPOINT, headers=HEADERS, params=params)
-    except requests.exceptions.RequestException as e:
-        logging.error('Ошибка соединения')
-        raise exceptions.GeneralErorrs(f'Ошибка соединения {e}') from e
+            url=ENDPOINT, headers=HEADERS, params=params)
+        if homework_statuses.status_code != HTTPStatus.OK:
+            raise exceptions.GeneralErorrs('Сбой работы сервера')
+        return homework_statuses.json()
     except JSONDecodeError as e:
-        logging.error('Ошибка формата данных')
         raise exceptions.GeneralErorrs(f'Ошибка формата данных {e}') from e
-    if homework_statuses.status_code != HTTPStatus.OK:
-        logging.error(
-            f'Сбой работы. Ответ сервера {homework_statuses.status_code}'
-        )
-        raise exceptions.GeneralErorrs('Сбой работы сервера')
-    return homework_statuses.json()
+    except requests.exceptions.RequestException as e:
+        raise exceptions.GeneralErorrs(f'Ошибка соединения {e}') from e
 
 
 def check_response(response):
     """Функция проверяет ответ API на корректность."""
-    try:
-        response['homeworks']
-        if type(response) is not dict:
-            raise exceptions.CommonErrors('Ответ вернулся не в виде словаря')
-        elif 'homeworks' or 'current_date' not in response:
-            raise exceptions.CommonErrors('Нужных ключей нет в словаре')
-    except exceptions.CommonErrors as ex:
-        if not isinstance(response['homeworks'], list):
-            raise TypeError('Запрос к серверу пришёл не в виде списка') from ex
+    if not isinstance(response, dict):
+        raise TypeError('Ответ вернулся не в виде словаря')
+    if 'homeworks' not in response:
+        raise exceptions.CommonErrors('Ключа homeworks нет в словаре')
+    if 'current_date' not in response:
+        raise exceptions.CommonErrors('Ключа current_date нет в словаре')
+    if not isinstance(response['homeworks'], list):
+        raise TypeError(
+            'Запрос к серверу пришёл не в виде списка')
     return response['homeworks']
 
 
 def parse_status(homeworks):
     """Функция извлекает информации домашней работе и ее статус."""
-    homework_name = homeworks['homework_name']
     if 'homework_name' not in homeworks:
         raise exceptions.CommonErrors(
             'Ключ homework_name не обнаружен в словаре')
-    homework_status = homeworks['status']
+    homework_name = homeworks['homework_name']
     if 'status' not in homeworks:
-        raise exceptions.CommonErrors('Ключ status не обнвружен в словаре')
-    if homework_status not in HOMEWORK_STATUSES:
+        raise exceptions.CommonErrors('Ключ status не обнаружен в словаре')
+    homework_status = homeworks.get('status')
+    if homework_status not in HOMEWORK_VERDICTS:
         raise exceptions.CommonErrors('Статус не обнаружен в списке')
     elif homework_status is None:
         raise exceptions.CommonErrors('Пришел пустой список')
-    verdict = HOMEWORK_STATUSES[homework_status]
+    verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -112,22 +107,24 @@ def main():
     """Основная логика работы бота."""
     if not check_tokens():
         logging.critical('Токены не доступны')
+        sys.exit()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
-    current_timestamp = int(time.time())
+    timestamp = int(time.time())
 
     while True:
         try:
-            response = get_api_answer(current_timestamp)
+            response = get_api_answer(timestamp)
+            # print(response)
             homeworks = check_response(response)
+            # print(homeworks)
             logging.info(f'Получен список работ {response}')
-            if type(homeworks) is list:
+            if homeworks is not None:
                 send_message(bot, parse_status(homeworks[0]))
             else:
                 logging.debug('Нет новых статусов')
                 send_message(bot, 'Нет новых статусов')
-                return False
-            current_timestamp = response.get('current_date', current_timestamp)
+            timestamp = response.get('current_date')
 
         except exceptions.GeneralErorrs as error:
             logging.error(f'{error}')
@@ -135,7 +132,7 @@ def main():
         except Exception as error:
             logging.error(f'{error}')
         finally:
-            time.sleep(RETRY_TIME)
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
